037d5e754198a4a5add1b95a4ee32b846ae7d9a6
Xalryth <30471411+Xalryth@users.noreply.github.com>
2019-01-28 12:42:55 +0100

    Initial commit

---
+++ src/Cards/Card.java
@@ -0,0 +1,17 @@
+package Cards;
+
+/*
+* @Author Peter C. Straarup 28/01-2019
+* Base class for the Card object,
+* which is extended upon to fulfill the requirements of other card types
+* */
+public abstract class Card {
+    Boolean faceUp;
+
+    public Card(){ }
+    public Card(Boolean faceUp){ this.faceUp = faceUp; }
+
+    public Boolean getFaceUp(){ return faceUp; }
+    public void setFaceUp(Boolean value){ faceUp = value; }
+    public void flipCard(){ faceUp = !faceUp; }
+}
---
+++ src/Cards/Deck.java
@@ -0,0 +1,9 @@
+package Cards;
+
+/*
+* @Author Peter C. Straarup 28/01-2019
+* Base Deck class for holding a collection of card
+* */
+public class Deck {
+
+}
